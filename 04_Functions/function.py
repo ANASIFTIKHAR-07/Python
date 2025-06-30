5e09095127cb949e6d662b8c07005259914e7d61
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-06-30 15:43:48 +0500

    Funs update

--- 04_Functions/function.py
+++ 04_Functions/function.py
@@ -44,22 +44,79 @@ def multiply(n1, n2) :
 
 #  04 Fucnction returning multiple values: The function will return both the area and circumference of the circle given its
 #  radius
-import math
+# import math
 
-def area_circumference(radius) :
-    area =  math.pi * radius ** 2
-    circumference = 2 * math.pi * radius
-    return area , circumference
+# def area_circumference(radius) :
+#     area =  math.pi * radius ** 2
+#     circumference = 2 * math.pi * radius
+#     return area , circumference
 
-input = int(input("Enter the radius to do the calculation: "))
+# input = int(input("Enter the radius to do the calculation: "))
 
-a , c = area_circumference(input)
+# a , c = area_circumference(input)
 
-print(f"The Area is: {a:.2f}, The cirumference is: {c:.2f}" )
-
-
+# print(f"The Area is: {a:.2f}, The cirumference is: {c:.2f}" )
 
 
+# 05 Defaule Parameter value: Write a function that greets a user. If no name is provided, it should greet with a defaule
+# name
+
+
+# def greet(name = "Anas"):
+#     return "Hello, " + name
+
+# print(greet("Ali"))
+
+
+# 06 Lamda function: Create a lambda function to compute the cube of a number
+#  Lamda functions are also known as anonymous function, one liners.
+
+# cube = lambda c: c** 3
+# print(cube(5))
+
+
+#  07 Variable length arguements:
+
+# def sum_all(*args) :
+#     print(args) # Returns the arguments in tuple object type
+#     for i in args:
+#         print(i * 2)
+#     return sum(args)
+
+# print(sum_all(1,2,3,4,5))
+
+
+#  08 ** kwargs multiple keyword values:
+#  Create a function that accepts any number of keywoard arguements and prints them in the format key : value.
+
+
+# def print_kwargs(**kwargs) :
+#     for key, value in kwargs.items():
+#         print(f"{key} : {value}")
+    
+# print_kwargs(name = "Benten", power = "Omnitrix", enemy = "Wilgax")
+
+
+#  09 Generate a function with yield
+#  Write a generator function thay yields even numbers up to a specified limit
+
+
+def even_generator(limit) :
+    for i in range(2 , limit + 1 , 2):
+        yield i
+ 
+for num in even_generator(10):
+    print(num)
+
+
+
+#  10 Recursive function :  Create a recursive function to calculate the factorial of a number.
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 
 
